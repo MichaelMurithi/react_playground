9e5e77ead17d69876c63dc8d7ee6c5b7097a790b
Myke <mykemitrichev@gmail.com>
2020-03-29 06:50:27 +0300

    Added gitignore, added a package.json, eslint configurations

--- src/app.js
+++ src/app.js
@@ -1,34 +1,28 @@
-const Pet = ({name,animal,breed})=> {
-   return React.createElement("div", {},[
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 };
 const App = () => {
-          return React.createElement(
-              "div",
-              {},[
-                React.createElement("h1",{},"Trial Successful"),
-                React.createElement(Pet,{
-                    name:"Luna",
-                    animal:"Dog",
-                    breed:"Havanese" 
-                }),
-                React.createElement(Pet,{
-                    name:"Pepper",
-                    animal:"Bird",
-                    breed:"Cocktail"
-                }),
-                React.createElement(Pet,{
-                    name:"Angella",
-                    animal:"Cat",
-                    breed:"Cocktail"
-                })
-              ]
-          );
-      };
-      ReactDOM.render(
-          React.createElement(App),
-          document.getElementById("root")
-      );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Trial Successful"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktail",
+    }),
+    React.createElement(Pet, {
+      name: "Angella",
+      animal: "Cat",
+      breed: "Cocktail",
+    }),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
