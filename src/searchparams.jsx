e9a6d1ee755197edf6d78e69cb109086a66834f1
Myke <mykemitrichev@gmail.com>
2020-03-29 11:33:38 +0300

    Included react hooks

--- src/searchparams.jsx
+++ src/searchparams.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import Pet from "./Pet";
+import React, { useState } from "react";
+//Best Practices while using hooks:
+/**
+ * A hook cannot be used in a loop/any control statement
+ *
+ */
 
+//creates the search component
 const SearchParams = () => {
-  const location = "Seattle,WA";
+  //assigns values from useState to default and current state
+  const [location, setLocation] = useState("Seattle,WA");
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
-          Location
           <input
             name="search"
             id="location"
             value={location}
             placeholder="Location"
+            //function to handle event change and settle a new location
+            onChange={(event) => setLocation(event.target.value)}
           ></input>
         </label>
         <button>Submit</button>
@@ -20,4 +28,4 @@ const SearchParams = () => {
     </div>
   );
 };
-export default SearchParams;
+export default SearchParams; //exports the component
