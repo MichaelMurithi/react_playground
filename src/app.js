c5b2d0a90c9b91a276ea46fe6df1d44680c89ce2
Myke <mykemitrichev@gmail.com>
2020-03-29 09:34:05 +0300

    Changed syntax to JSX

--- src/app.js
+++ src/app.js
@@ -3,23 +3,13 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Trial Successful"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocktail",
-    }),
-    React.createElement(Pet, {
-      name: "Angella",
-      animal: "Cat",
-      breed: "Cocktail",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+      <Pet name="Pepper" animal="Bird" breed="Cocktail"></Pet>
+      <Pet name="Angella" animal="Cat" breed="Havanese"></Pet>
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
