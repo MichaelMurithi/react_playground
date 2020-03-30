e765e6e871aa8576665d01dcfceda83a9e8ee24b
Myke <mykemitrichev@gmail.com>
2020-03-30 14:53:38 +0300

    Added Reach router

--- src/Details.js
+++ src/Details.js
@@ -3,3 +3,4 @@ import React from "react";
 const Details = () => {
   return <h1>Hello Myke</h1>;
 };
+export default Details;
--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-import { Reach } from "@reach/router";
+import Details from "./Details";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./searchparams";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
