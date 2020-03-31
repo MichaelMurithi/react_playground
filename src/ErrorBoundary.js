48ecd70fb24d549a39dc97687aea24d7e8c7e4e7
Myke <mykemitrichev@gmail.com>
2020-03-31 03:39:03 +0300

    Added ErrorBoundary component

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+        hasError = false
+    }
+  }
+  
+  render(){}
+}
