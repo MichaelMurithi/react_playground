f7dc120946a092c9a8d756413bfeb988c935cb15
Myke <mykemitrichev@gmail.com>
2020-04-23 13:47:57 +0300

    multiple styls using backticks

--- concepts_intro/basics/src/components/Stylesheet.js
+++ concepts_intro/basics/src/components/Stylesheet.js
@@ -4,7 +4,7 @@ function Stylesheet(props) {
   let className = props.primary ? "primary" : " "; //Styles the component as of class primary based on parameters given
   return (
     <div>
-      <h1 className={className}>Stylesheets</h1>
+      <h1 className={`${className}, font-xl`}>Stylesheets</h1>
     </div>
   );
 }
