365aa0f65cf6c1d4a019b152553df1d1578934f2
Myke <mykemitrichev@gmail.com>
2020-04-23 14:10:30 +0300

    performed inline styling

---
+++ concepts_intro/basics/src/components/Inline.js
@@ -0,0 +1,14 @@
+import React from "react";
+const heading = {
+  color: "brown",
+  fontSize: "1.5em"
+};
+function Inline() {
+  return (
+    <div>
+      <h1 style={heading}>Inline</h1>
+    </div>
+  );
+}
+
+export default Inline;
