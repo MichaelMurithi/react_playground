d58c800e0359c005eba9980aca35294be2106fb8
Myke <mykemitrichev@gmail.com>
2020-04-23 13:31:56 +0300

    styled using css modules

---
+++ concepts_intro/basics/src/components/Stylesheet.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./myStyles.css"; //Imports the stylesheet
+function Stylesheet() {
+  return (
+    <div>
+      <h1 className="primary">Stylesheets</h1>
+    </div>
+  );
+}
+
+export default Stylesheet;
