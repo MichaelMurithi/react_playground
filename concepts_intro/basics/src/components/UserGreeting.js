760d2f43ee77a442e2c60d26df3371fb3516beee
Myke <mykemitrichev@gmail.com>
2020-04-23 12:16:11 +0300

    Conditional rendering using short-circuit

--- concepts_intro/basics/src/components/UserGreeting.js
+++ concepts_intro/basics/src/components/UserGreeting.js
@@ -10,12 +10,15 @@ class UserGreeting extends Component {
   }
 
   render() {
-    //Using Ternary
-    return this.state.isLoggedIn ? (
-      <h3>Welcome User</h3>
-    ) : (
-      <h3>Welcome Guest</h3>
-    );
+    //Using short_circuiting
+    return this.state.isLoggedIn && <h3>Welcome Myke</h3>;
+
+    // //Using Ternary
+    // return this.state.isLoggedIn ? (
+    //   <h3>Welcome User</h3>
+    // ) : (
+    //   <h3>Welcome Guest</h3>
+    // );
     //Cunditional rendering using if..else
     // if (this.state.isLoggedIn) {
     //   return <div>Welcome Myke</div>;
