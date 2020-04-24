6c08f59831a404811551ff572d52e08a8b3369b0
Myke <mykemitrichev@gmail.com>
2020-04-24 04:21:06 +0300

    Implemented react fragment

---
+++ concepts_intro/basics/src/components/FragmentsDemo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FragmentsDemo() {
+  return (
+    <React.Fragment>
+      <h3>Fragment Demo</h3>
+      <p>This is an implementation of react fragments</p>
+    </React.Fragment>
+  );
+}
+
+export default FragmentsDemo;
