f290de9caecad5d1ef7e71ceef009616c0ec9338
Myke <mykemitrichev@gmail.com>
2020-05-05 11:43:46 +0300

    Implemented data fetching using useReducer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,27 +1,11 @@
 import React, { useReducer } from "react";
 import "./App.css";
-import MultipleReducers from "./components/useReducer/MultipleReducers";
-const initialCount = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increament":
-      return state + 1;
-    case "decreament":
-      return state - 1;
-    case "reset":
-      return initialCount;
-    default:
-      return state;
-  }
-};
-export const CountContext = React.createContext();
+import DataFetching from "./components/useReducer/DataFetching";
+
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialCount);
   return (
     <div className="App">
-      <CountContext.Provider value={{ count: count, dispatch: dispatch }}>
-        <MultipleReducers />
-      </CountContext.Provider>
+      <DataFetching />
     </div>
   );
 }
