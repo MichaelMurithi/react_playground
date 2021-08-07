bf886b955f246c7b28709e9a5ddc8f671881a710
Michael Murithi <mykemitrichev@gmail.com>
2021-08-07 22:23:26 +0200

    learn(react-native): Learnt how to define buttons with simple alerts as event handlers

--- react-native/DoneWithIt/App.tsx
+++ react-native/DoneWithIt/App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Image, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	Image,
+	SafeAreaView,
+	TouchableWithoutFeedback,
+	Button,
+	Alert
+} from 'react-native';
 
 export default function App() {
 	const handlePress = (message: string) => console.log(message);
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text numberOfLines={1} onPress={() => handlePress('Hello text pressed')}>
+			{/* <Text numberOfLines={1} onPress={() => handlePress('Hello text pressed')}>
 				Hello User, from Done With It app!
 			</Text>
 			<TouchableWithoutFeedback onPress={() => handlePress('Image Pressed')}>
@@ -18,7 +26,20 @@ export default function App() {
 						height: 200
 					}}
 				/>
-			</TouchableWithoutFeedback>
+			</TouchableWithoutFeedback> */}
+			<Button
+				color='blue'
+				title='Change Text'
+				onPress={() =>
+					Alert.alert('Confirm Action', ' Are you sure you want to do this ?', [
+						{ text: ' Yes', onPress: () => console.log('Yes, they are sure :(') },
+						{
+							text: ' No',
+							onPress: () => console.log('Nop, they dont know what they are doing :(')
+						}
+					])
+				}
+			/>
 			<StatusBar style='auto' />
 		</SafeAreaView>
 	);
