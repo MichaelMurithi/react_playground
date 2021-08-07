051f6cc3b4c755a7db85c71fb7dc6b9343b6e1fe
Michael Murithi <mykemitrichev@gmail.com>
2021-08-07 23:40:46 +0200

    learn(react-native): Used dimensions and orientation hook to modify styles based on orientation

--- react-native/DoneWithIt/App.tsx
+++ react-native/DoneWithIt/App.tsx
@@ -1,47 +1,21 @@
 import React from 'react';
-import {
-	StyleSheet,
-	Text,
-	Image,
-	SafeAreaView,
-	TouchableWithoutFeedback,
-	Button,
-	Alert,
-	Platform,
-	StatusBar
-} from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, StatusBar, View, Dimensions } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+/* 
+- We can get the Dimensions of a screen using the dimensions API
 
+*/
 export default function App() {
-	const handlePress = (message: string) => console.log(message);
+	console.log(Dimensions.get('screen'));
+	const { landscape } = useDeviceOrientation();
 	return (
-		<SafeAreaView style={[styles.container, containerStyle]}>
-			<Text numberOfLines={1} onPress={() => handlePress('Hello text pressed')}>
-				Hello User, from Done With It app!
-			</Text>
-			<TouchableWithoutFeedback onPress={() => handlePress('Image Pressed')}>
-				<Image
-					fadeDuration={1000}
-					source={{
-						uri: 'https://picsum.photos/200/200',
-						width: 200,
-						height: 200
-					}}
-				/>
-			</TouchableWithoutFeedback>
-			<Button
-				color='blue'
-				title='Change Text'
-				onPress={() =>
-					Alert.alert('Confirm Action', ' Are you sure you want to do this ?', [
-						{ text: ' Yes', onPress: () => console.log('Yes, they are sure :(') },
-						{
-							text: ' No',
-							onPress: () => console.log('Nop, they dont know what they are doing :(')
-						}
-					])
-				}
-			/>
-			<StatusBar />
+		<SafeAreaView style={styles.container}>
+			<View
+				style={{
+					backgroundColor: 'black',
+					width: '100%',
+					height: landscape ? '100%' : '30%'
+				}}></View>
 		</SafeAreaView>
 	);
 }
@@ -51,12 +25,10 @@ export default function App() {
  * This makes the stylesheet api better in defining styles
  * When multiple style classes are provided, the object on the right overides the properties of the object on the left
  */
-const containerStyle = { backgroundColor: 'orange' };
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
 		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
 	}
 });
