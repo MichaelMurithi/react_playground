88f06b52fdce6c70ddc53a3af5067f43926367fb
Michael Murithi <mykemitrichev@gmail.com>
2021-08-07 22:34:20 +0200

    learn(react-native): Learnt how to use the stylesheet api to define styles

--- react-native/DoneWithIt/App.tsx
+++ react-native/DoneWithIt/App.tsx
@@ -13,8 +13,8 @@ import {
 export default function App() {
 	const handlePress = (message: string) => console.log(message);
 	return (
-		<SafeAreaView style={styles.container}>
-			{/* <Text numberOfLines={1} onPress={() => handlePress('Hello text pressed')}>
+		<SafeAreaView style={[styles.container, containerStyle]}>
+			<Text numberOfLines={1} onPress={() => handlePress('Hello text pressed')}>
 				Hello User, from Done With It app!
 			</Text>
 			<TouchableWithoutFeedback onPress={() => handlePress('Image Pressed')}>
@@ -26,7 +26,7 @@ export default function App() {
 						height: 200
 					}}
 				/>
-			</TouchableWithoutFeedback> */}
+			</TouchableWithoutFeedback>
 			<Button
 				color='blue'
 				title='Change Text'
@@ -45,6 +45,12 @@ export default function App() {
 	);
 }
 
+/**
+ * Stylesheet.create validates the styling properties passed to it
+ * This makes the stylesheet api better in defining styles
+ * When multiple style classes are provided, the object on the right overides the properties of the object on the left
+ */
+const containerStyle = { backgroundColor: 'orange' };
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
